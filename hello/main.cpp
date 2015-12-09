e41241916b0966c603b38ace6450eb0497656545
Azhar Aulia Saputra <Azhar@Azhar-MacPro.local>
2015-12-09 15:48:30 +0900

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Azhar Aulia Saputra on 12/9/15.
+//  Copyright (c) 2015 Azhar Aulia Saputra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
